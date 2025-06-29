ac3117431d1b7b66d0d2f63415369ff1a926ea2d
Kalindu Tharanga <kalindu.th@gmail.com>
2025-06-29 16:01:01 +0530

    Day02 part01 completed code

--- Day02/advent_of_code_Day_02_01.py
+++ Day02/advent_of_code_Day_02_01.py
@@ -3,6 +3,7 @@ f = open('input.txt', 'r')
 content = f.read()
 f.close()
 
+
 def add_to_arr(content):
 
     data_arr = []
@@ -11,10 +12,45 @@ def add_to_arr(content):
     for line in data:
         row = line.split()
         data_arr.append(row)
-    
-    print(data_arr[100])
 
-add_to_arr(content)
+    return data_arr
+    
+arr = add_to_arr(content)
+
+def is_safe(arr):
+
+    count = 0
+
+    for row in arr:
+
+        if len(row) != len(set(row)):
+            continue
+        
+        try:
+            nums = list(map(int, row))
+        except ValueError:
+            nums = row
+        
+        is_increasing = all(i < j for i, j in zip(nums, nums[1:]))
+        is_decreasing = all(i > j for i, j in zip(nums, nums[1:]))
+
+        if not (is_increasing or is_decreasing):
+            continue
+
+        differences_valid = all(1 <= abs(j - i) <= 3 for i, j in zip(nums, nums[1:]))
+
+        if differences_valid:
+            count += 1
+        
+    return count
+            
+c = is_safe(arr)
+
+print(c)
+
+is_safe(arr)
+#def is_safe():
+
 
 
     
