1a7fff38870bba00bd00e5846e65e090a5e719f7
Kalindu Tharanga <kalindu.th@gmail.com>
2025-06-29 18:11:41 +0530

    Part 02 start

--- Day02/advent_of_code_Day_02_01.py
+++ Day02/advent_of_code_Day_02_01.py
@@ -14,23 +14,20 @@ def add_to_arr(content):
         data_arr.append(row)
 
     return data_arr
-    
+
+
 arr = add_to_arr(content)
 
 def is_safe(arr):
 
     count = 0
-
     for row in arr:
-
         if len(row) != len(set(row)):
             continue
-        
         try:
             nums = list(map(int, row))
         except ValueError:
             nums = row
-        
         is_increasing = all(i < j for i, j in zip(nums, nums[1:]))
         is_decreasing = all(i > j for i, j in zip(nums, nums[1:]))
 
@@ -41,11 +38,10 @@ def is_safe(arr):
 
         if differences_valid:
             count += 1
-        
     return count
-            
-c = is_safe(arr)
 
+        
+c = is_safe(arr)
 print(c)
 
 is_safe(arr)
